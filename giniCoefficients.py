43da7c1519ecb669a0bff1f8f094fd76f2c95219
BFauber <BPFauber@gmail.com>
2025-02-18 09:27:57 -0600

    Update giniCoefficients.py

--- giniCoefficients.py
+++ giniCoefficients.py
@@ -76,6 +76,10 @@ def calc_gini_by_row(embeddings: np.ndarray, normalize: bool=True) -> np.ndarray
     Calculates all-versus-all similarities of np.ndarrray (n x d) of embeddings.
 
     NOTE: ASSUMES all embeddings are L2-NORMALIZED.
+
+    Uses memory-efficient approach of calculating similarity and Gini by row.
+    This approach is likely slower than memory-intensive calculation of 
+    all similarities before calculating the Gini coefficients.
     
     `Normalize` is a MinMax scaling of outputs such that [0,1] for all Gini values.
     
@@ -103,7 +107,77 @@ def calc_gini_by_row(embeddings: np.ndarray, normalize: bool=True) -> np.ndarray
     if normalize:
         scaler = MinMaxScaler()
         scaler.fit(gini_array)
-        gini_array = scaler.transform(gini_array).flatten()
-    return gini_array
+        gini_array = scaler.transform(gini_array)
+    return gini_array.flatten()
 
 
+def calc_gini_all(embeddings: np.ndarray, normalize: bool) -> np.ndarray:
+    """
+    Calculates all-versus-all similarities of a size (n x d)
+    np.arrray of embeddings.
+
+    NOTE: ASSUMES all embeddings are L2-NORMALIZED.
+    
+    Uses time-efficient approach of calculating similarity for all elements
+    first, followed by the Gini calculation.
+    This approach is likely faster but more memory-intensive than calculating 
+    per-row similarities and Gini coefficients.
+    
+    `Normalize` is a MinMax scaling of outputs such that [0,1] for all Gini values.
+    
+    Input:
+    ------
+    embeddings : np.ndarray of n x d dimensions *NOTE: ASSUMES ALL EMBEDDINGS ARE
+        L2-NORMALIZED
+    normalize : bool {default=True} enables or disables the `MinMax` scaling of
+        the resulting Gini coefficients to [0,1]
+    
+    Output:
+    -------
+    array : np.ndarray of n x 1 dimensions of the Gini coefficients of 
+        the input array
+    """
+    gini_list = []
+    i = 0
+    n = embeddings.shape[0]
+    all_similarities = np.matmul(embeddings, embeddings.T)
+    all_similarities = 1 - all_similarities
+    all_similarities[all_similarities < 0.0] = 0.0
+    while i < n:
+        gini = gini_coefficient_numpy(all_similarities[i, :])
+        gini_list.append(gini)
+        i += 1
+    gini_array = np.array(gini_list).reshape(-1, 1)
+    if normalize:
+        scaler = MinMaxScaler()
+        scaler.fit(gini_array)
+        gini_array = scaler.transform(gini_array)
+    return gini_array.flatten()
+
+
+def giniCoefficient(embeddings: np.ndarray, normalize: bool=True, memory_efficient: bool=True) -> np.ndarray:
+    """
+    Calculates all-versus-all similarities of np.ndarrray (n x d) of embeddings.
+    
+    `Normalize` is a MinMax scaling of outputs such that [0,1] for all values.
+    
+    Input:
+    ------
+    embeddings : np.ndarray of n x d dimensions
+    normalize : bool {default=True} enables or disables the `MinMax` scaling of
+        the resulting Gini coefficients to [0,1]
+    memory_efficient : bool {default=True} enables or disables the memory-efficient
+        per-row similarity and Gini coefficient calculation instead of the more
+        time-efficient yet memory-intensive similarity calculation for all elements 
+        followed by the Gini calculation.
+    
+    Output:
+    -------
+    array : np.ndarray of n x 1 dimensions of the Gini coefficients of 
+        the input array
+    """
+    if memory_efficient:
+        result = calc_gini_by_row(embeddings, normalize)
+    else:
+        result = calc_gini_all(embeddings, normalize)
+    return result
